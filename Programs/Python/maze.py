a59b4632444005e0c67565bfda1d0a58dd9f5845
ShotaroBaba <bshotaro@gmail.com>
2019-05-24 22:27:51 +1000

    Began to create Maze game

---
+++ Programs/Python/maze.py
@@ -0,0 +1,17 @@
+import os, pygame
+from pygame.locals import *
+from pygame.compat import geterror
+import time
+
+# This game will become Role Playing Game where a person 
+# walks around the maze and fights with foes.
+
+
+# TODO: Create the algorithm for generating maze.
+def generate_maze_grid():
+    pass
+
+# TODO: Putting codes in the main program.
+def main():
+    pass
+
