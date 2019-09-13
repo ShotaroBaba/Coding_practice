3f158e078a70c104ec2c34ceaea7f282daa7acea
ShotaroBaba <bshotaro@gmail.com>
2019-09-13 23:03:11 +1000

    Move class to other file & fixed bug.

--- Programs/Python/python_games/lib/default_values.py
+++ Programs/Python/python_games/lib/default_values.py
@@ -1,3 +1,6 @@
 
 data_dir = "data"
 monster_data_file_name = "creature_list.json"
+
+save_data_dir = "save_data.json"
+save_data_folder = "save_data"
--- Programs/Python/python_games/maze_rpg_game.py
+++ Programs/Python/python_games/maze_rpg_game.py
@@ -15,6 +15,8 @@ from lib.getch import _Getch
 from lib.clear_screen import clear
 from lib.maze_generation import generate_maze_grid, make_maze_grid
 from random import random
+from lib.maze_object import MazeObject
+
 from lib.default_values import *
 
 constant_next_level_exp = 1.4
@@ -60,20 +62,7 @@ def load_data():
 def save_data():
     pass
 
-# Used for returning a certain value for a certain situation.
-# The enemy strength is decided randomly.
-def return_json_value_data(key_value, default_value, json_data, level  = 1, is_random = "no"):
-    if json_data != {}:
-        if is_random == "yes":
-            return round(json_data[key_value] * uniform(level-0.2, level + 0.2), 0)
-        else:
-            return json_data[key_value]
-    else:
-        return default_value
 
-    # return json_data[key_value] \
-    #     if json_data != {} else json_data[key_value] * default_value * random.uniform(level-1, level) \
-    #         if is_random == "yes" and json_data != {} else default_value
 
 
 # Used for a player, an enemy and a non-player character class
@@ -81,208 +70,7 @@ def return_json_value_data(key_value, default_value, json_data, level  = 1, is_r
 
 
 
-class MazeObject(object):
 
-    # Read the json object to initalise the data, 
-    def __init__(self, json_data = {}, level = 1, is_random = "no"):
-        
-        # The enemy and player level, which is an important value.
-        self.level = json_data["level"] if json_data != {} else 1
-
-        self.bonus_point = json_data["bonus"] if json_data !={} and "bonus" in json_data.keys() else 0
-        
-        #Note: maximum points available
-        # hp: Hit point
-        # mp: Magic point
-        # sp: Stamina point
-        # ep: Energy point
-        self.hp = return_json_value_data("hp", 100, json_data, level, is_random)
-        self.mp = return_json_value_data("mp", 100, json_data, level, is_random)
-        self.sp = return_json_value_data("sp", 100, json_data, level, is_random)
-        self.ep = return_json_value_data("ep", 100, json_data, level, is_random)
-
-        self.current_hp = None
-        self.current_mp = None
-        self.current_sp = None
-        self.current_ep = None
-
-
-        # Object's parameters
-        self.strength = return_json_value_data("strength", 10, json_data, level, is_random)
-        self.agility = return_json_value_data("agility", 10, json_data, level, is_random)
-        self.vitality = return_json_value_data("vitality", 10, json_data, level, is_random)
-        self.dexterity = return_json_value_data("dexterity", 10, json_data, level, is_random)
-
-        self.smartness = return_json_value_data("smartness", 10, json_data, level, is_random)
-        self.magic_power = return_json_value_data("magic_power", 10, json_data, level, is_random)
-        self.mental_strength = return_json_value_data("mental_strength", 10, json_data, level, is_random)
-
-        # TODO: Implement luckiness effects
-        self.luckiness = return_json_value_data("luckiness", 10, json_data, level, is_random)
-
-        # Check whether it is a player, an enemy or just an object
-        self.player_name = json_data["player_name"]\
-            if json_data != {} and "player_name" in json_data.keys() else "None"
-        self.is_living = json_data["is_living"]\
-            if json_data != {} and "is_living" in json_data.keys( ) else True
-        self.is_player = json_data["is_player"]\
-            if json_data != {} and "is_player" in json_data.keys() else True
-        self.is_enemy = json_data["is_enemy"]\
-            if json_data != {} and "is_enemy" in json_data.keys() else  False
-
-        # Show the objects that player wields.
-        self.right_arm = json_data["right_arm"]\
-            if json_data != {} and "right_arm" in json_data.keys() else "Empty"
-        self.left_arm = json_data["left_arm"] if\
-            json_data != {} and "left_arm" in json_data.keys() else "Empty"
-
-        self.head = json_data["head"]\
-            if json_data != {} and "is_enemy" in json_data.keys() else "Empty"
-        self.arm =  json_data["arm"]\
-            if json_data != {} and "arm" in json_data.keys() else "Empty"
-        self.leg = json_data["leg"]\
-            if json_data != {} and "leg" in json_data.keys() else "Empty"
-        self.wrist = json_data["wrist"]\
-            if json_data != {} and "wrist" in json_data.keys() else "Empty"
-
-        self.right_finger = json_data["right_finger"]\
-            if json_data != {} and "right_finger" in json_data.keys() else "Empty"
-        self.left_finger = json_data["left_finger"]\
-            if json_data != {} and "left_finger" in json_data.keys() else "Empty"
-
-        # Initialise paramteres based on player's equipment.
-        self._init_parameters_equipment()
-
-        # Status is normal by default
-        # if the status is normal, then the player is not affected.
-        self.status = json_data["status"]\
-            if json_data != {} and "status" in json_data.keys() else "Normal"
-
-        # By default, the Objects's position is unknown.
-        # It is only applied to the player.
-        self.object_pos = json_data["object_pos"]\
-            if json_data != {} and "object_pos" in json_data.keys() else None
-
-        # Check how the character is displayed.
-        self.displayed_character = json_data["displayed_character"]\
-            if json_data != {} and "displayed_character" in json_data.keys() else "@"
-
-        # The maximum number of items that Objects can hold
-        # It can be increased by the level up.
-        self.max_item_hold = json_data["max_item_hold"]\
-            if json_data != {} and "max_item_hold" in json_data.keys() else 10
-
-        # Objects's experience point when gaining the points
-        self.exp = int(round(json_data["exp"] * uniform(level-0.2, level+0.2),0))\
-            if json_data != {} and "exp" in json_data.keys() else 0
-
-        # Object's current expereince points if available
-        # This is only for the player...
-        self.current_exp = json_data["current_exp"]\
-            if json_data != {} and "current_exp" in json_data.keys() else 0
-
-        # This is only for the player...
-        self.bonus_point = json_data["bonus_point"]\
-            if json_data != {} and "bonus_point" in json_data.keys() else 0
-
-        # Objects's necessary experience points.
-        self.next_exp = json_data["next_exp"]\
-            if json_data != {} and "next_exp" in json_data.keys() else 50
-
-        # Item numbers is recorded in the dictionary type
-        self.items = json_data["items"]\
-            if json_data != {} and "items" in json_data.keys() else {}
-
-        # TODO Create the skill classes for users
-        self.skills = json_data["skills"]\
-            if json_data != {} and "skills" in json_data.keys() else {}
-
-        # Rank that can be used for adjusting the random encounter in dungeon.
-        self.rank = json_data["rank"]\
-            if json_data != {} and "rank" in json_data.keys() else {}
-
-        # The monster will drop the item
-        self.rank = json_data["drop_item"]\
-            if json_data != {} and "drop_item" in json_data.keys() else {}
-
-        # Initial skill for attacking the enemy.
-        self.basic_attack = lambda x: 10 * self.strength * random.uniform(0.8, 1.0)
-
-    # Apply skills to a certain person, enemy or items
-    # NOTE: Skills will be a set of json data.
-    def use_skills(self, skill_name, target):
-        self.skills[skill_name].activate_skills(target)
-        pass
-
-    # Adjust parameters based on the equipment
-    def _init_parameters_equipment(self):
-        pass
-
-    # Experience gained after the battles or through the items.
-    def gained_exp(self):
-        pass
-
-    # Player can wear his or her own weapon, accessory or armor
-    def attach_item(self, item):
-        pass
-
-    # Player can remove his or her own weapon, accessory or armor
-    def detach_item(self, item):
-        pass
-
-    def _initialise_json(self):
-        pass
-
-    # Initialise player when the game is started.
-    def initialise_player(self):
-        pass
-
-
-    # Used for generating json data for saving character's data.
-    def return_character_data_json(self):
-        pass
-
-    # Move characters when the enemy
-    def move_object_up(self, key_event):
-        pass
-
-    def move_object_down(self, key_event):
-        pass
-
-    def move_object_left(self, key_event):
-        pass
-    
-    def move_object_right(self, key_event):
-        pass
-    
-    # The system of the calculation of the level
-    def get_experience(self, exp_values):
-        self.current_exp += exp_values
-
-        self.next_exp -= exp_values
-
-        while self.next_exp < 1:
-            
-            remain = self.next_exp
-            self.bonus_point += 5 + self.level // 5
-            self.level += 1
-            print("Player level up!")
-            print("You gained {} bonus points.".format(self.bonus_point))
-            print("Player reached to level {}".format(self.level))
-            
-            for _ in range(3 + (self.level // 10)):
-                tmp_choice = choice(numerical_player_strengh + non_numerical_player_strength)
-                # Randomly allocate the values once the level is up
-                if tmp_choice in numerical_player_strengh:
-                    exec("""self.{} += 5""".format(tmp_choice))
-                    print("The {} value increases by 5!".format(tmp_choice))
-                else:
-                    exec("""self.{} += 1""".format(tmp_choice))
-                    print("The {} value increases by 1!".format(tmp_choice))
-
-                
-            # The formula for calculating the next level.
-            self.next_exp = 50 + int(round(50 * (0.5 * (self.level ** (constant_next_level_exp))))) - remain
 
         
 # This includes passive skills and active skills used by player, enemy and objects.
@@ -385,10 +173,10 @@ class Map(object):
 
         self.draw_map()
     
-        self.manipulate_map()
+        self._manipulate_map_main_menu()
 
     
-    def manipulate_map(self):
+    def _manipulate_map_main_menu(self):
         while True:
             character = getch()
             
@@ -398,6 +186,8 @@ class Map(object):
             else:
                 if character in ["UP_KEY", "DOWN_KEY", "LEFT_KEY", "RIGHT_KEY"]:
                     self.move_player(character)
+                elif character == b"\x1b":
+                    self.player_menu()
 
 
     # Turn-based fight is now imminent
@@ -418,19 +208,6 @@ class Map(object):
         
         random_enemy = choice(list(enemy_json.keys()))
         enemy = MazeObject(json_data = enemy_json[random_enemy], level = self.level, is_random="yes")
-
-        # Temporary breaking point for testing program
-        # while True:
-        #     clear()
-
-        #     print(random_enemy)
-        #     print("This is only the test fight.")
-        #     print("The material will be added later on...")
-        #     # TODO: Create the fight screen between enemy's
-        #     character = getch()
-        #     if character == b"n":
-        #         clear()
-        #         break
 
 
         def player_turn_normal_attack():
@@ -503,14 +280,12 @@ class Map(object):
                     if uniform(0.8, 1.0)*self.player.agility > uniform(0.8,1.0)* enemy.agility:
                         if player_turn_normal_attack():
                             break
-
                         if enemy_turn_normal_attack():
                             break
 
                     else:
                         if enemy_turn_normal_attack():
                             break
-                        
                         if player_turn_normal_attack():
                             break
                     
@@ -623,12 +398,62 @@ class Map(object):
 
 
     def display_status(self):
-        print("HP: {}, MP: {}, SP: {}, EP: {}".format(self.player.hp, self.player.mp, self.player.sp, self.player.ep))
-        
+        print("HP: {}, MP: {}, SP: {}, EP: {}".format(self.player.current_hp, 
+        self.player.current_mp,
+        self.player.current_sp, 
+        self.player.current_ep))
     
 
     def player_menu(self):
-        print("")
+        clear()
+        
+        selection_list = ["Item", "Save", "Exit"]
+        cursor_not_selected = " "
+        cursor_selected = ">"
+        tmp_cursor = deepcopy(selection_list)
+        cursor_selection = 1
+        
+        while True:
+            for i in range(len(tmp_cursor)):
+                if i == cursor_selection:
+                    tmp_cursor[i] = cursor_selected + tmp_cursor[i]
+                else:
+                    tmp_cursor[i] = cursor_not_selected + tmp_cursor[i]
+            
+            print("\n".join(tmp_cursor))
+
+            tmp_cursor = deepcopy(selection_list)
+            tmp = getch()
+            if tmp == "UP_KEY":
+                if cursor_selection > 0:
+                    cursor_selection -= 1
+                
+            elif tmp == "DOWN_KEY":
+                if cursor_selection < len(tmp_cursor) - 1:
+                    cursor_selection += 1
+
+            elif tmp == b"\r":
+                # Yes case --> Initialise map.
+                if cursor_selection == 0:
+                    break
+                
+                # Next TODO: To create player data save.
+                # No case --> Do nothing.
+                if cursor_selection == 1:
+                    break
+                
+                # 
+                if cursor_selection == 2:
+                    break
+            
+            elif tmp == b"\x1b":
+                clear()
+                self.draw_map()
+                break
+
+            clear()
+
+        
         pass
 
     # It is called every time the cursor is moved.
@@ -666,7 +491,8 @@ class Map(object):
         else:
             clear()
             self.draw_map()
-            
+    
+    # Appears when pressing escape button.
 
 
     # Randomly place player
